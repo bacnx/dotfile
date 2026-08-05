3739482ff447eb93ae202ea5a1407a5b637c47e9
bacnx <nxbac.dev@gmail.com>
2026-08-05 13:48:56 +0700

    feat(nvim): search hidden files in telescope by default
    find_files/live_grep now pass --hidden, so .env, .github/ and other
    dotfiles show up while .gitignore is still respected. Add <leader>fa and
    <leader>fG as no-ignore variants for the rare case dist/ or node_modules
    needs searching.
    
    Also fix file_ignore_patterns: '.git' was an unescaped Lua pattern that
    matched any character before "git" (e.g. foo.gitignore).

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,12 +1,20 @@
 require('telescope').setup({
   defaults = {
     layout_strategy = 'horizontal',
-    file_ignore_patterns = { 'node_modules', '.git' },
+    file_ignore_patterns = { 'node_modules/', '%.git/' },
     mappings = {
       i = {
         ['<C-k>'] = 'move_selection_previous',
         ['<C-j>'] = 'move_selection_next',
       },
+    },
+  },
+  pickers = {
+    find_files = { hidden = true },
+    live_grep = {
+      additional_args = function()
+        return { '--hidden' }
+      end,
     },
   },
   extensions = {
@@ -22,6 +30,20 @@ local builtin = require('telescope.builtin')
 keymap('n', '<leader>ff', builtin.find_files, { desc = 'Find files' })
 keymap('n', '<leader>fg', builtin.live_grep,  { desc = 'Live grep' })
 keymap('n', '<leader>fb', builtin.buffers,    { desc = 'Buffers' })
+
+-- Same pickers, but ignoring .gitignore (dist/, build/, node_modules/, ...)
+keymap('n', '<leader>fa', function()
+  builtin.find_files({
+    hidden = true,
+    no_ignore = true,
+    file_ignore_patterns = { '%.git/' },
+  })
+end, { desc = 'Find files (no ignore)' })
+keymap('n', '<leader>fG', function()
+  builtin.live_grep({ additional_args = function()
+    return { '--hidden', '--no-ignore' }
+  end })
+end, { desc = 'Live grep (no ignore)' })
 
 require('telescope').load_extension('file_browser')
 keymap('n', '<leader>fs', function()
